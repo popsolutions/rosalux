cd309e18c5ab5e0288dd86573419d0d1dc12c4d0
sdobke <sdobke@gmail.com>
2021-09-02 14:50:51 -0300

    New html for livro based on single-livro

--- wp-content/themes/betheme-child/woocommerce/content-single-product.php
+++ wp-content/themes/betheme-child/woocommerce/content-single-product.php
@@ -135,99 +135,67 @@ if ( ! defined( 'ABSPATH' ) ) {
 				?>
 				<?php //echo $echo_author; ?>
 				<h1><?php the_title(); ?></h1>
-				<?php echo $echo_ano.$echo_author.$echo_editora; ?>
+				<?php echo $echo_author.$echo_ano.$echo_editora; ?>
 			</div>
 		</div>
 		<?php
 		remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
 		?>
 
-		<div class="column one-second product_image_wrapper">
-			<?php
-				/**
-				 * woocommerce_before_single_product_summary hook.
-				 *
-				 * @hooked woocommerce_show_product_sale_flash - 10
-				 * @hooked woocommerce_show_product_images - 20
-				 */
-				do_action( 'woocommerce_before_single_product_summary' );	
-				?>
-			</div>
+		<div class="post-wrapper-content single_content">
 
-			<div class="summary entry-summary column one-second">
-				<?php echo $echo_file;?>
-				<?php
-				/**
-				 * woocommerce_single_product_summary hook.
-				 *
-				 * @hooked woocommerce_template_single_title - 5
-				 * @hooked woocommerce_template_single_rating - 10
-				 * @hooked woocommerce_template_single_price - 10
-				 * @hooked woocommerce_template_single_excerpt - 20
-				 * @hooked woocommerce_template_single_add_to_cart - 30
-				 * @hooked woocommerce_template_single_meta - 40
-				 * @hooked woocommerce_template_single_sharing - 50
-				 * @hooked WC_Structured_Data::generate_product_data() - 60
-				 */
-				do_action( 'woocommerce_single_product_summary' );
-				?>
+    <div class="post_content">
+        <div class="d-flex mb-4">
+          <?php 
+          if($thumb){ 
+          ?>
+            <div class="book-thumb">
+              <img src="<?php echo $thumb; ?>">
+              <img src="<?php echo $thumb2; ?>" style="display: none">
+            </div>
+          <?php  
+          }
+          ?>
+          <div class="pl-3 book-desc">
+            <?php 
+              the_content(); 
+              echo $echo_file;
+            ?>
+          </div>
+        </div>
+    </div>
+    
+		<!-- <?php //if( mfn_opts_get( 'share' ) && ( get_post_meta( get_the_ID(), 'mfn-post-template', true ) == 'intro' ) ): ?>
+			<div class="section section-post-intro-share">
+				<div class="section_wrapper clearfix">
+					<div class="column one">
 
-				<?php 
-				// Description | Default - right column
-				if( in_array( $single_product_style, array( 'wide', 'wide tabs') ) ) {
-					remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10);
-				}
-				
-				remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_upsell_display', 15 );
-				remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20);
-				?>
+						<div class="share_wrapper clearfix">
+							<span class='st_facebook_vcount' displayText='Facebook'></span>
+							<span class='st_twitter_vcount' displayText='Tweet'></span>
+							<span class='st_pinterest_vcount' displayText='Pinterest'></span>						
+							
+							<script src="http<?php //mfn_ssl(1); ?>://w<?php //mfn_ssl(1); ?>.sharethis.com/button/buttons.js"></script>
+							<script>stLight.options({publisher: "1390eb48-c3c3-409a-903a-ca202d50de91", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
+						</div>
 
-				<?php
-				/**
-				 * woocommerce_after_single_product_summary hook.
-				 *
-				 * @hooked woocommerce_output_product_data_tabs - 10
-				 * @hooked woocommerce_upsell_display - 15
-				 * @hooked woocommerce_output_related_products - 20
-				 */
-				do_action( 'woocommerce_after_single_product_summary' );
-				?>
-
-			</div>
-
-			<?php if( mfn_opts_get( 'share' ) ): ?>
-				<div class="share_wrapper">
-					<span class='st_facebook_vcount' displayText='Facebook'></span>
-					<span class='st_twitter_vcount' displayText='Tweet'></span>
-					<span class='st_pinterest_vcount' displayText='Pinterest'></span>
-
-					<script src="http<?php mfn_ssl(1); ?>://w<?php mfn_ssl(1); ?>.sharethis.com/button/buttons.js"></script>
-					<script>stLight.options({publisher: "1390eb48-c3c3-409a-903a-ca202d50de91", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
+					</div>
 				</div>
-			<?php endif; ?>
-
+			</div>
+		<?php //endif; ?> -->
+	</div>
+	
+	<?php if( mfn_opts_get( 'blog-comments' ) ): ?>
+		<div class="section section-post-comments">
+			<div class="section_wrapper clearfix">
+			
+				<div class="column one comments">
+					<?php comments_template( '', true ); ?>
+				</div>
+				
+			</div>
 		</div>
+	<?php endif; ?>
 
 
-		<?php 
-		// Description | Default - wide below image
-		if( in_array( $single_product_style, array( 'wide', 'wide tabs') ) ) {
-			woocommerce_output_product_data_tabs(); 
-		}
-		?>
-
-
-		<?php 
-		woocommerce_upsell_display();
-		if( mfn_opts_get( 'shop-related' ) ) woocommerce_output_related_products(); 
-		?>
-
-
-		<?php if( version_compare( WC_VERSION, '2.7', '<' ) ): ?>
-			<meta itemprop="url" content="<?php the_permalink(); ?>" />
-		<?php endif; ?>
-
-
-	</div><!-- #product-<?php the_ID(); ?> -->
-
-	<?php do_action( 'woocommerce_after_single_product' ); ?>
+	
