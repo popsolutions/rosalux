2dd3de73cba383d2853114a57d979ad174e50590
sdobke <sdobke@gmail.com>
2021-09-02 14:43:35 -0300

    Download button for livro

--- wp-content/themes/betheme-child/woocommerce/content-single-product.php
+++ wp-content/themes/betheme-child/woocommerce/content-single-product.php
@@ -45,14 +45,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 	
 	$opts_single_post_nav = mfn_opts_get( 'prev-next-nav' );
 	if( is_array( $opts_single_post_nav ) ){
-	
+
 		if( isset( $opts_single_post_nav['hide-header'] ) ){
 			$single_post_nav['hide-header'] = true;
 		}
 		if( isset( $opts_single_post_nav['hide-sticky'] ) ){
 			$single_post_nav['hide-sticky'] = true;
 		}
-	
+
 	}
 	
 	$post_prev = get_adjacent_post( false, '', true );
@@ -77,7 +77,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	
 	$single_product_style = mfn_opts_get( 'shop-product-style' );
 	$classes[] = $single_product_style;
-	 
+
 	
 	// translate
 	$translate['all'] = mfn_opts_get('translate') ? mfn_opts_get('translate-all','Show all') : __('Show all','betheme');
@@ -89,60 +89,58 @@ if ( ! defined( 'ABSPATH' ) ) {
 	} else {
 		$product_schema = '';
 	}
-?>
+	?>
 
-<div <?php echo $product_schema; ?> id="product-<?php the_ID(); ?>" <?php post_class( $classes ); ?>>
-	
-	<?php 
+	<div <?php echo $product_schema; ?> id="product-<?php the_ID(); ?>" <?php post_class( $classes ); ?>>
+
+		<?php 
 		// single post navigation | sticky
 		if( ! $single_post_nav['hide-sticky'] ){
 			echo mfn_post_navigation_sticky( $post_prev, 'prev', 'icon-left-open-big' ); 
 			echo mfn_post_navigation_sticky( $post_next, 'next', 'icon-right-open-big' );
 		} 
-	?>
+		?>
 		
-	<?php 
+		<?php 
 		// single post navigation | header
 		if( ! $single_post_nav['hide-header'] ){
 			echo mfn_post_navigation_header( $post_prev, $post_next, $shop_page_id, $translate );
 		}
-	?>
-
-	
+		?>
 
 
-	<div class="post-wrapper-content">
-		<div class="post-header">
-		<?php
-			$id = get_the_ID();
-			$author = get_post_meta($id, 'tp_livro_autor');
-			$echo_author = $author ? '<h3>'.$author[0].'</h3>' : '';
 
-			$editora = get_post_meta($id, 'tp_livro_editora');
-			$echo_editora = $editora ? '<h4> - '.$editora[0].'</h4>' : '';
 
-			$ano = get_post_meta($id, 'tp_livro_ano');
-			$echo_ano = $ano ? '<h4>'.$ano[0].'</h4>' : '';
+		<div class="post-wrapper-content">
+			<div class="post-header">
+				<?php
+				$id = get_the_ID();
+				$author = get_post_meta($id, 'tp_livro_autor');
+				$echo_author = $author ? '<h3>'.$author[0].'</h3>' : '';
 
-			$file = get_post_meta($id, 'tp_livro_file');
-			$echo_file = $file ? '<a class="button float-right btn-sm p-2 px-4" href="'.$file[0].'" target="_blank">BAIXAR</a>' : '';
-			
-			$thumb = get_the_post_thumbnail_url($id, 'full');
+				$editora = get_post_meta($id, 'tp_livro_editora');
+				$echo_editora = $editora ? '<h4> - '.$editora[0].'</h4>' : '';
 
-			$thumb2 = get_the_post_thumbnail_url($id, array(395, 222));
+				$ano = get_post_meta($id, 'tp_livro_ano');
+				$echo_ano = $ano ? '<h4>'.$ano[0].'</h4>' : '';
 
-			
-		  ?>
-		  <?php //echo $echo_author; ?>
-		  <h1><?php the_title(); ?></h1>
-		  <?php echo $echo_ano.$echo_editora; ?>
-	  </div>
-  </div>
-		<?php
-			remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
+				$file = get_post_meta($id, 'tp_livro_file');
+				$echo_file = $file ? '<a class="button float-right btn-sm p-2 px-4" href="'.$file[0].'" target="_blank">BAIXAR</a>' : '';
 
-			 do_action( 'woocommerce_template_single_title' );?>
+				$thumb = get_the_post_thumbnail_url($id, 'full');
+
+				$thumb2 = get_the_post_thumbnail_url($id, array(395, 222));
+
+
+				?>
+				<?php //echo $echo_author; ?>
+				<h1><?php the_title(); ?></h1>
+				<?php echo $echo_ano.$echo_author.$echo_editora; ?>
+			</div>
 		</div>
+		<?php
+		remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
+		?>
 
 		<div class="column one-second product_image_wrapper">
 			<?php
@@ -153,38 +151,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 				 * @hooked woocommerce_show_product_images - 20
 				 */
 				do_action( 'woocommerce_before_single_product_summary' );	
-			?>
-		</div>
-	
-		<div class="summary entry-summary column one-second">
-			<div class="post-wrapper-content">
-	  <div class="post-header">
-		  <?php
-			$id = get_the_ID();
-			$author = get_post_meta($id, 'tp_livro_autor');
-			$echo_author = $author ? '<h3>'.$author[0].'</h3>' : '';
+				?>
+			</div>
 
-			$editora = get_post_meta($id, 'tp_livro_editora');
-			$echo_editora = $editora ? '<h4> - '.$editora[0].'</h4>' : '';
-
-			$ano = get_post_meta($id, 'tp_livro_ano');
-			$echo_ano = $ano ? '<h4>'.$ano[0].'</h4>' : '';
-
-			$file = get_post_meta($id, 'tp_livro_file');
-			$echo_file = $file ? '<a class="button float-right btn-sm p-2 px-4" href="'.$file[0].'" target="_blank">BAIXAR</a>' : '';
-			
-			$thumb = get_the_post_thumbnail_url($id, 'full');
-
-			$thumb2 = get_the_post_thumbnail_url($id, array(395, 222));
-
-			
-		  ?>
-		  <?php //echo $echo_author; ?>
-		  <h1><?php the_title(); ?></h1>
-		  <?php echo $echo_ano.$echo_editora; ?>
-	  </div>
-  </div>
-			<?php
+			<div class="summary entry-summary column one-second">
+				<?php echo $echo_file;?>
+				<?php
 				/**
 				 * woocommerce_single_product_summary hook.
 				 *
@@ -198,9 +170,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 				 * @hooked WC_Structured_Data::generate_product_data() - 60
 				 */
 				do_action( 'woocommerce_single_product_summary' );
-			?>
-			
-			<?php 
+				?>
+
+				<?php 
 				// Description | Default - right column
 				if( in_array( $single_product_style, array( 'wide', 'wide tabs') ) ) {
 					remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10);
@@ -208,9 +180,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 				
 				remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_upsell_display', 15 );
 				remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20);
-			?>
-			
-			<?php
+				?>
+
+				<?php
 				/**
 				 * woocommerce_after_single_product_summary hook.
 				 *
@@ -219,43 +191,43 @@ if ( ! defined( 'ABSPATH' ) ) {
 				 * @hooked woocommerce_output_related_products - 20
 				 */
 				do_action( 'woocommerce_after_single_product_summary' );
-			?>
-	
-		</div>
-		
-		<?php if( mfn_opts_get( 'share' ) ): ?>
-			<div class="share_wrapper">
-				<span class='st_facebook_vcount' displayText='Facebook'></span>
-				<span class='st_twitter_vcount' displayText='Tweet'></span>
-				<span class='st_pinterest_vcount' displayText='Pinterest'></span>
-				
-				<script src="http<?php mfn_ssl(1); ?>://w<?php mfn_ssl(1); ?>.sharethis.com/button/buttons.js"></script>
-				<script>stLight.options({publisher: "1390eb48-c3c3-409a-903a-ca202d50de91", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
+				?>
+
 			</div>
-		<?php endif; ?>
-		
-	</div>
-	
-	
-	<?php 
+
+			<?php if( mfn_opts_get( 'share' ) ): ?>
+				<div class="share_wrapper">
+					<span class='st_facebook_vcount' displayText='Facebook'></span>
+					<span class='st_twitter_vcount' displayText='Tweet'></span>
+					<span class='st_pinterest_vcount' displayText='Pinterest'></span>
+
+					<script src="http<?php mfn_ssl(1); ?>://w<?php mfn_ssl(1); ?>.sharethis.com/button/buttons.js"></script>
+					<script>stLight.options({publisher: "1390eb48-c3c3-409a-903a-ca202d50de91", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
+				</div>
+			<?php endif; ?>
+
+		</div>
+
+
+		<?php 
 		// Description | Default - wide below image
 		if( in_array( $single_product_style, array( 'wide', 'wide tabs') ) ) {
 			woocommerce_output_product_data_tabs(); 
 		}
-	?>
-	
-	
-	<?php 
+		?>
+
+
+		<?php 
 		woocommerce_upsell_display();
 		if( mfn_opts_get( 'shop-related' ) ) woocommerce_output_related_products(); 
-	?>
-	
-	
-	<?php if( version_compare( WC_VERSION, '2.7', '<' ) ): ?>
-		<meta itemprop="url" content="<?php the_permalink(); ?>" />
-	<?php endif; ?>
+		?>
 
 
-</div><!-- #product-<?php the_ID(); ?> -->
+		<?php if( version_compare( WC_VERSION, '2.7', '<' ) ): ?>
+			<meta itemprop="url" content="<?php the_permalink(); ?>" />
+		<?php endif; ?>
 
-<?php do_action( 'woocommerce_after_single_product' ); ?>
+
+	</div><!-- #product-<?php the_ID(); ?> -->
+
+	<?php do_action( 'woocommerce_after_single_product' ); ?>
