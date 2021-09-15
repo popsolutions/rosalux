c5fa13bfaa89eeccfdead20c86ca4547cc638aa6
sdobke <sdobke@gmail.com>
2021-09-15 18:03:00 -0300

    search results show stock for testing purposes

--- wp-content/themes/betheme-child/functions.php
+++ wp-content/themes/betheme-child/functions.php
@@ -1401,6 +1401,7 @@ function format_results_midiateca($_args){
 					$cats[] = $category->name;
 				}
 			}
+			global $product;
 			$chapeu = get_post_meta($id, 'chapeu');
 			$post['categories'] = $cats;
 			$post['title'] = get_the_title();
@@ -1408,6 +1409,7 @@ function format_results_midiateca($_args){
 			$post['excerpt'] = get_the_excerpt($id);
 			$post['link'] = get_permalink();
 			$post['date'] = get_the_date();
+			$post['stock'] = $product->get_stock_quantity();
 			$post['chapeu'] = $chapeu ? $chapeu[0] : '';
 			$res[0][] = $post;
 			$res[1][] = $id;
