a670cf0b7777b238a59f596e6d0d2ca631623ef0
sdobke <sdobke@gmail.com>
2023-06-15 16:18:52 -0300

    Update functions.php

--- wp-content/themes/betheme-child/functions.php
+++ wp-content/themes/betheme-child/functions.php
@@ -2051,7 +2051,8 @@ add_shortcode('print-menu', 'cetweb_print_custom_menu_shortcode');
 function add_slug_body_class( $classes ) {
 	global $post;
 	if ( isset( $post ) ) {
-	$classes[] = $post->post_type . '-' . $post->post_name;
+	$classes[] = $post->post_type;
+	$classes[] = $post->post_name;
 	}
 	return $classes;
 	}
